50751860931088930f1d335e0f37f366c0f8ea7b
IsmaelRuge <ismaelruge@gmail.com>
2024-09-19 20:40:14 -0500

    Add Contactenos

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,27 @@
+// Inicializa Contactenos
+let formContact = document.getElementById('formContact');
+let nameContact = document.getElementById('nameContact');
+let emailContact = document.getElementById('emailContact');
+let messageContact = document.getElementById('messageContact');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.service-images img');
+formContact.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    images.forEach(image => {
-        image.addEventListener('click', () => {
-            alert(`Has hecho clic en la imagen: ${image.alt}`);
-        });
-    });
+    let dtn = {
+        nombre: nameContact.value,
+        correo: emailContact.value,
+        mensaje: messageContact.value
+    };
+
+    alert(`Hola ${dtn.nombre}, se te enviará un correo a ${dtn.correo} cuando nuestros asesores hayan analizado tu mensaje, muchas gracias.`);
 });
+
+// Finaliza Contactenos
+
+// Inicializa BtnCompras
+
+async function btnCompras(params) {
+    alert(`haz comprado el articulo ${params}`);
+}
+
+// Finaliza Compras
